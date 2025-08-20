72ef746da16d5d3187e2392b9c1b6806cd57c19e
grider25 <garrettrider4@gmail.com>
2025-08-20 13:43:51 -0400

    Amend the function, add some comments

--- Scripts/Cleaning/00_summarizeTalons_0820.R
+++ Scripts/Cleaning/00_summarizeTalons_0820.R
@@ -1,36 +1,42 @@
 ##############
-#
-#
-#
-#
-#
+# ITRE Talons/TFMSC comparisons
+# Programmed by: Garrett Rider
+# Programmed to: clean talons data from 2024
+# Program date: 8/20/2025
 ###############
 
 library(itRePACKage)
 library(tidyverse)
+
+
 getDrivePath('01_EconPolicy_Aviation', '01_Misc Data and NCDoA Asks/9999 Talons_TFMSC', 'gdrider')->drivePath
-
 inputPath <- '/05 - Data/ncTalons2024/'
+outputPath <- './06 - Tasks and Analysis/01_R Output/'
 
-list.files(paste0(drivePath, inputPath), pattern='talons')->talonsFiles
 
-
-summarize_talons <- function(index){
-read.csv(paste0(drivePath, inputPath, talonsFiles[index]))->ds
-
-stringr::str_split_fixed(ds$day, '\\-', 3) -> separateYear
-aptname <- str_sub(talonsFiles[index], 7, 9)
-
-ds2 <- ds %>% 
-  mutate(
-    year =separateYear[,1],
-    month = separateYear[,2],
-    day = separateYear[,3]
+#Function to summarize TALONS data for all contributing airports in 1200.aero
+summarize_talons <- function(extension){
+  
+  #read the data in using the paths
+  read.csv(paste0(drivePath, inputPath,extension))->ds
+  
+  #split the date to get year, month, day
+  stringr::str_split_fixed(ds$day, '\\-', 3) -> separateYear
+  
+  #because of how the files are named, this is easy: we can pull the 3-letter code (it's all the same)
+  aptname <- str_sub(extension, 7, 9)
+  
+  #add the components of the dates as separate variables
+  ds2 <- ds %>% 
+    mutate(
+      year =separateYear[,1],
+      month = separateYear[,2],
+      day = separateYear[,3]
     ) %>% 
-  select(-day)
-
-
-talonsByMonth <-  ds2 %>% 
+    select(-day)
+  
+  #
+  talonsByMonth <-  ds2 %>% 
     group_by(month) %>% 
     summarize(landings=sum(landings),
               takeoffs=sum(takeoffs),
@@ -39,11 +45,21 @@ talonsByMonth <-  ds2 %>%
               avg_wind_speed = mean(na.omit(avg_wind_speed)),
               avg_temp = mean(na.omit(avg_temp))) %>% 
     mutate(airport = aptname)
- 
-return(talonsByMonth) 
+  
+  return(talonsByMonth) 
 }
 
 
-for(i in 1:10){
-  print(summarize_talons(i))
+#all files starting with talons: these are from 1200.aero
+list.files(paste0(drivePath, inputPath), pattern='talons')->talonsFiles
+
+#set empty
+talons <- NA
+
+#loop through the folder
+for(i in 1:length(talonsFiles)){
+
+  #add to dataset 
+ talons <- rbind(talons, summarize_talons(talonsFiles[i]))
 }
+talons <- talons[-1,]
