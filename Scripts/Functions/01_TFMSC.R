661fdcccbc33b7edf0faef647e244db6d5d2ec4f
grider25 <garrettrider4@gmail.com>
2025-08-21 19:28:31 -0400

    Add comments, revise functions, remove redundancies

--- Scripts/Functions/01_TFMSC.R
+++ Scripts/Functions/01_TFMSC.R
@@ -10,8 +10,8 @@ inputPath <- '/05 - Data/'
 outputPath <- './06 - Tasks and Analysis/01_R Output/'
 
 
-summarizeTFMSC_withJets <- function(projectPath, inputPath, fileName){
-read.csv(paste0(projectPath, inputPath, fileName), skip=3) ->ds
+summarizeTFMSC_withJets <- function(filepath){
+read.csv(filepath, skip=3) ->ds
 
 ds <- (ds[,-1])
 
@@ -61,8 +61,8 @@ return(list(foo, monthlyOps_byAirport, annualOps_byAirport))
 
 
 
-summarizeTFMSC_withADG <- function(projectPath, inputPath, fileName){
-  read.csv(paste0(projectPath, inputPath, fileName), skip=3) ->ds
+summarizeTFMSC_withADG <- function(filepath){
+  read.csv((filepath), skip=3) ->ds
   
   ds <- (ds[,-1])
   
@@ -80,34 +80,34 @@ summarizeTFMSC_withADG <- function(projectPath, inputPath, fileName){
   
   
   foo <- ds %>% 
+    
+    #remove all blank entries for the airport (totals, anything else)
     filter(airport != '') %>% 
-    select(airport,
-           date,
-           adg, 
-           total_ops) %>% 
+
+    #split date in two to get momnth and year, adjust the total operations to be numeric
     mutate(month = stringr::str_split_i(date, '\\-',1),
            year = stringr::str_split_i(date, '\\-', 2),
            total_ops = as.numeric(total_ops)) %>% 
-    select(airport,
-           month, 
-           year,
-           adg,
-           total_ops) %>% 
+    
+    #pivot the ADGs to get one column per ADG type with the corresponding visit counts in each
    pivot_wider(names_from = 'adg', 
                values_from = 'total_ops',
                values_fill=0) %>% 
-    mutate(total = `No Data`+ I+II+III+ IV+ VI)
+    #mutate again to get the total number of operations
+    mutate(total = `No Data`+ I+II+III+ IV+ VI) %>% 
+    select(airport, 
+           month,
+           year, 
+           `No Data`,
+           I, 
+           II, 
+           III, 
+           IV, 
+           VI, 
+           total)
 
-  monthlyOps_byAirport <- foo %>%
-   group_by(airport, month) %>% 
-    summarize(no_data = sum(`No Data`),
-              sumI = sum(I),
-              sumII = sum(II),
-              sumIII = sum(III),
-              sumIV = sum(IV),
-              sumVI = sum(VI),
-              total_ops = sum(total)) 
   
+  #to get operations on an annual basis, we do the same thing except group by just airport, not month
   annualOps_byAirport <- foo %>% 
     group_by(airport) %>% 
     summarize(no_data = sum(`No Data`),
@@ -119,14 +119,15 @@ summarizeTFMSC_withADG <- function(projectPath, inputPath, fileName){
               total_ops = sum(total))
   
   #return(foo)
-  return(list(foo, monthlyOps_byAirport, annualOps_byAirport))
+  return(list(foo, annualOps_byAirport))
 }
 
 
-summarizeTFMSC_withJets(drivePath, inputPath, '2024NCGAA_TFMSC.csv')-> summary_2024
-summarizeTFMSC_withJets(drivePath, inputPath, 'TFMSC-Report-80368.csv')-> summary_other
+summarizeTFMSC_withJets(paste0(drivePath, inputPath, '2024NCGAA_TFMSC.csv'))-> summary_2024
+summarizeTFMSC_withJets(paste0(drivePath, inputPath, 'TFMSC-Report-80368.csv'))-> summary_other
 
-summarizeTFMSC_withADG(drivePath, inputPath, 'TFMSC-Report-45806.csv')
+summarizeTFMSC_withADG(paste0(drivePath, inputPath, 'TFMSC-Report-45806.csv'))
+
 read_csv(paste0(drivePath, inputPath, '2024NCGAA_TFMSC.csv'))->z
 
 z <- summary_other[[2]]
